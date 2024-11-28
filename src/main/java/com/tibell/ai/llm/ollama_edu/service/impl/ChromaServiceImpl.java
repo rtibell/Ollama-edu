28aa1a34205e3e07b8c0d26f517cd1f35b90af3b
Rasmus Tibell <rasmus.tibell@gmail.com>
2024-11-28 22:20:16 +0100

    Add ReadMe file.

--- src/main/java/com/tibell/ai/llm/ollama_edu/service/impl/ChromaServiceImpl.java
+++ src/main/java/com/tibell/ai/llm/ollama_edu/service/impl/ChromaServiceImpl.java
@@ -1,20 +1,17 @@
 package com.tibell.ai.llm.ollama_edu.service.impl;
 
-import com.tibell.ai.llm.ollama_edu.config.MilvusConfig;
+import com.tibell.ai.llm.ollama_edu.config.ChromeConfig;
 import com.tibell.ai.llm.ollama_edu.service.MilvusService;
-import io.milvus.client.MilvusClient;
-import io.milvus.v2.client.ConnectConfig;
 import io.milvus.v2.client.MilvusClientV2;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 public class MilvusServiceImpl implements MilvusService {
     @Autowired
-    private MilvusConfig config;
+    private ChromeConfig config;
 
     @Autowired
     private MilvusClientV2 milvusClientV2;
